050ea185e655faf5ebca88d6e5d7e385b395b17a
Mujakkir85 <mujakkirlucse@gmail.com>
2022-04-04 03:30:47 +0600

    Home page image, heading, pharagraph added

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import BMW from './BMW.jpg';
+import './Home.css'
 
 const Home = () => {
     return (
         <div>
-            <h2>From home page</h2>
+            <div className='home-head'>
+                <div className='head-text'>
+                    <h1>BMW <span>007</span></h1>
+                    <h1>Get The James <span>Bond BMW</span></h1>
+                    <p>The acronym BMW stands for Bayerische Motoren Werke GmbH, which roughly translates to the Bavarian Engine Works Company. The name harks back to the company’s origin in the German state of Bavaria. It also indicates BMW's original product range: engines for various applications.</p>
+                </div>
+                <div className='head-img'>
+                    <img className='bmw-img' src={BMW} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
