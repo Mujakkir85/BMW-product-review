e5903654ee1a326ef3d00c146481b97924d89c31
Mujakkir85 <mujakkirlucse@gmail.com>
2022-04-04 16:49:40 +0600

    chart added in dashboard

--- src/component/Dashboard/Dashboard.js
+++ src/component/Dashboard/Dashboard.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Area, AreaChart, CartesianGrid, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+import './Dashboard.css'
 
 const Dashboard = () => {
+
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setData(data))
+    }, [])
     return (
-        <div>
-            <h2>From Dashboard</h2>
+        <div className='chart'>
+            <div>
+                <h2>Line Chart</h2>
+                <LineChart width={500} height={400} data={data} >
+                    <Line dataKey={'sell'}></Line>
+                    <Line dataKey={'investment'}></Line>
+                    {/* <Line dataKey={'revenue'}></Line> */}
+                    <XAxis dataKey={'month'}></XAxis>
+                    <YAxis ></YAxis>
+                    <Tooltip></Tooltip>
+                </LineChart>
+            </div>
+            <br />
+
+            <div>
+                <h2>Pie Chart</h2>
+                <AreaChart
+                    width={500}
+                    height={400}
+                    data={data}
+                    margin={{
+                        top: 10,
+                        right: 30,
+                        left: 0,
+                        bottom: 0,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="sell" stackId="1" stroke="#8884d8" fill="#8884d8" />
+                    <Area type="monotone" dataKey="investment" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+                    <Area type="monotone" dataKey="revenue" stackId="1" stroke="#ffc658" fill="#ffc658" />
+                </AreaChart>
+            </div>
+
         </div>
+
     );
 };
 
